2b3f133c0525a098fb16782a4517eaf59fd4363f
nathaniamnbe <nathanael1084@gmail.com>
2025-08-21 15:44:25 +0700

    Update server.mjs

--- opname/backend/server.mjs
+++ opname/backend/server.mjs
@@ -36,7 +36,7 @@ cloudinary.config({
 // 5. Otentikasi Google (HANYA untuk Sheets)
 const serviceAccountAuth = new JWT({
   email: process.env.GOOGLE_SERVICE_ACCOUNT_EMAIL,
-  key: process.env.GOOGLE_PRIVATE_KEY.replace(/\\n/g, "\n"),
+  key: process.env.GOOGLE_PRIVATE_KEY, // Langsung gunakan variabelnya
   scopes: ["https://www.googleapis.com/auth/spreadsheets"],
 });
 const doc = new GoogleSpreadsheet(
